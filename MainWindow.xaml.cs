382db19d549386cbb29bd4cad145c2b815ec6f84
jphellemons <info@jphellemons.nl>
2014-03-05 15:03:56 +0100

    first commit
    fc

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Coding4Fun.Obd.ObdManager;
+
+namespace DetroitFocus
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObdDevice obd;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            obd = new ObdDevice();
+            obd.ObdChanged += _obd_ObdChanged;
+            
+            obd.Connect("COM1", 115200, ObdDevice.UnknownProtocol, true);
+
+            Dictionary<int,List<int>> sPids = obd.GetSupportedPids();
+
+            foreach(var sp in sPids.Keys)
+            {
+                log.Text += "Key : " + sp.ToString() + Environment.NewLine;
+                List<int> keyValues = new List<int>();
+                if (sPids.TryGetValue((int)sp, out keyValues))
+                {
+                    foreach (var kv in keyValues)
+                    {
+                        log.Text += "Value : " + kv.ToString() + Environment.NewLine;
+                    }
+                }
+            }
+        }
+
+        private void _obd_ObdChanged(object sender, ObdChangedEventArgs e)
+        {
+            log.Text += e.ObdState.ToString() + Environment.NewLine;
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Project Detroit for Ford Focus #
+
+This project is to check out the OBD-II support for a Ford Focus
+To learn more about Project Detroit, refer to these links:
+
+ - [Codeplex](https://obd.codeplex.com)
+ - [Coding4Fun](http://channel9.msdn.com/coding4fun/articles/Project-Detroit-How-to-Read-Your-Cars-Engine-Data-with-OBD-II)
+ - [Channel9 blog](http://channel9.msdn.com/Blogs/Vector/The-400-horsepower-device) 
+ - [Dutch Focus club](http://www.focusclub.nl/forum/)
